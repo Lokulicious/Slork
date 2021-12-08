4f2ca6c73b6110109744f156e05218fde508b9ad
Nick Bohnsack <nick.bohnsack@gmail.com>
2021-12-09 00:42:04 +0100

    Imported Sensory Toolkit, Removed Polarith

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SensorToolkit;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public SteeringRig steering;
+
+    void Start()
+    {
+    
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
