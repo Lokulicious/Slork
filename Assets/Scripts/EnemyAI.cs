0155497064be5e3199e3c4afc7ba9c80f058783f
Nick Bohnsack <nick.bohnsack@gmail.com>
2021-12-11 01:52:25 +0100

    Added Nearest Player Detection

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,24 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using SensorToolkit;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
 
-    public SteeringRig steering;
+    [SerializeField] NavMeshAgent agent;
 
-    void Start()
+    [SerializeField] GameObject player;
+
+    ArrayList players = new ArrayList();
+
+    [SerializeField] float detectionRange;
+
+
+    private void Start()
     {
-    
-        
-
-
+        players.Add(player);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if(Vector3.Distance(transform.position, getNearestTarget(agent, players).transform.position) <= detectionRange)
+        {
+            moveAgentToObject(agent, player);
+        }
+
     }
+
+
+    void moveAgentToObject(NavMeshAgent _agent, GameObject target)
+    {
+        //_agent.SetDestination(target.transform.position);
+        _agent.SetDestination(getNearestTarget(agent, players).transform.position);
+    }
+
+
+
+
+
+    GameObject getNearestTarget(NavMeshAgent _agent, ArrayList targets)
+    {
+        GameObject nearestTarget = null;
+
+        foreach (GameObject target in targets)
+        {
+            if (nearestTarget == null)
+            {
+                nearestTarget = target;
+            }
+            else if (Vector3.Distance(target.transform.position, _agent.transform.position) < Vector3.Distance(nearestTarget.transform.position, _agent.transform.position))
+            {
+                nearestTarget = target;
+            }
+        }
+
+        return nearestTarget;
+    }
+
+
+
 }
